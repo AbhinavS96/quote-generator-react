e6939a12ccd1c2f88b68a6c91a7dcf4d2ddfb45f
AbhinavS96 <abhinavsidharth@gmail.com>
2022-01-04 22:38:42 +0530

    added functionality

--- src/quote.js
+++ src/quote.js
@@ -1,26 +1,59 @@
-function Quote() {
-  if (true) {
-    return (
-      <div className="container">
-        <div className="quote-container">
-          <i className="fas fa-quote-left"></i>
-          <span className="quote"></span>
+import React, { Component } from "react";
+
+class Quote extends Component {
+  constructor() {
+    super();
+    this.state = { quote: "", author: "", quotes: [] };
+  }
+  newQuote = () => {
+    let { text, author } =
+      this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
+    this.setState({
+      quote: text,
+      author: author,
+    });
+  };
+  componentDidMount() {
+    // let quote = getQuotes();
+    // console.log(quote);
+    // this.setState({ quote: quote });
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((quotes) => {
+        this.setState({
+          quotes: quotes,
+        });
+        this.newQuote();
+      });
+  }
+  render() {
+    if (this.state.quote) {
+      return (
+        <div className="container">
+          <div className="quote-container">
+            <i className="fas fa-quote-left"></i>
+            <span className="quote">{this.state.quote}</span>
+          </div>
+          <div className="author-container">
+            <span className="author">{this.state.author}</span>
+          </div>
+          <div className="button-container">
+            <button className="button-twitter" title="share">
+              <i className="fab fa-twitter"></i>
+            </button>
+            <button
+              className="button-quote"
+              title="new quote"
+              onClick={this.newQuote}
+            >
+              New quote
+            </button>
+          </div>
         </div>
-        <div className="author-container">
-          <span className="author"></span>
-        </div>
-        <div className="button-container">
-          <button className="button-twitter" title="share">
-            <i className="fab fa-twitter"></i>
-          </button>
-          <button className="button-quote" title="new quote">
-            New quote
-          </button>
-        </div>
-      </div>
-    );
-  } else {
-    return <div className="loader"></div>;
+      );
+    } else {
+      return <div className="loader"></div>;
+    }
   }
 }
 
